060ebd0bff26fe8b140b4054bc50fc5661d25a49
S <srivatsachich@gmail.com>
2022-06-16 22:40:44 +0530

    arrays

---
+++ src/main/kotlin/DataStructures/Arrays.kt
@@ -0,0 +1,13 @@
+package DataStructures
+
+fun main(args:Array<String>) {
+//    var first = Array<Char>(5, (Int) -> T))
+//    first[0] = 's'
+//    first[1] = 'r'
+//    for (i in 2..first.size) first[i] = '0'
+//    println(first)
+
+    var myarr = arrayOf(1,2,3,4,5, true, 'C', "I am Blue")
+    println(myarr)
+
+}
